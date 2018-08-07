880f83b82f390c943aa56265867c97f33140e949
thekthuser <thekthuser@thekthuser.com>
2018-08-07 14:47:14 -0400

    general parts of ProfileForm

--- onboarding/services/forms.py
+++ onboarding/services/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField, validators
+from wtforms import SelectField, StringField, validators
+from wtforms.fields.html5 import EmailField
 from onboarding.models import Profile
 
 class ProfileForm(Form):
@@ -7,3 +8,8 @@ class ProfileForm(Form):
     model = Profile
 
   owner_name = StringField('Your name: ', [validators.DataRequired()])
+  pet_name = StringField('Your pet\'s name: ', [validators.DataRequired()])
+  zip_code = StringField('Zipcode: ', [validators.DataRequired()])
+  email = EmailField('Email: ', [validators.DataRequired(), validators.Email()])
+  breed = StringField('Your pet\'s breed: ', [validators.DataRequired()])
+  gender = SelectField('Your pet\'s gender: ', choices=zip(Profile.GENDERS.keys(), Profile.GENDERS.values()))
