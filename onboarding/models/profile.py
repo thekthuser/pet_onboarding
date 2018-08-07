23cd7331be56d654e75d74be4341cad3688c9c05
thekthuser <thekthuser@thekthuser.com>
2018-08-07 12:10:53 -0400

    some Profile model, including enum

--- onboarding/models/profile.py
+++ onboarding/models/profile.py
@@ -2,5 +2,12 @@ from onboarding import db
 
 class Profile(db.Model):
   __tablename__ = u'profiles'
+  GENDERS = {u'MALE': u'Male', u'FEMALE': u'Female'}
+
   id = db.Column(db.Integer, primary_key=True)
   owner_name = db.Column(db.Unicode(128), default=None)
+  pet_name = db.Column(db.Unicode(128), default=None)
+  zip_code = db.Column(db.Unicode(16), default=None)
+  email = db.Column(db.Unicode(128), default=None)
+  breed = db.Column(db.Unicode(64), default=None)
+  gender = db.Column(db.Enum(*GENDERS.values(), name=u'profile_genders'))
